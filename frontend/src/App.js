bdd6298ddb1fea5e6343da5272311cba802cac34
RISING-CODER777 <85391175+RISING-CODER777@users.noreply.github.com>
2023-08-27 19:54:19 +0530

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <head>
+        <title>HR Dashboard</title>
+      </head>
+      <iframe
+        title="HR Dashboard"
+        width="1140"
+        height="541.25"
+        src="https://app.powerbi.com/reportEmbed?reportId=e1a54d8f-639f-498c-b12c-67ef87bf7d3b&autoAuth=true&ctid=3e19468b-e065-44a1-9cde-749457855c06"
+        frameBorder="0"
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
+}
+
+export default App;
